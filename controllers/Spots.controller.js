ad8a23cfc4805c588b2b182312a9b1f1f99e120d
RAFAEL SCHMIDTL <rafael.schmidtl@senacsp.edu.br>
2025-06-30 21:15:31 -0300

    quarto commit

--- controllers/Spots.controller.js
+++ controllers/Spots.controller.js
@@ -5,11 +5,18 @@ const store = async (req,res) =>{
     const { company, price, techs } = req.body
     const { user_id } = req.headers;
 
+    if (!req.file)
+        return res.status(400).json({error: 'Arquivo não encontrado'})
+
+    const { filename } = req.file;
+
+    console.log(req.file);
+
     const usuario = await User.findById(user_id);
-    if(!usuario) 
-return res.status(400).json({error:'Usuáro não existe!!!'})
+    if(!usuario) return res.status(400).json({error:'Usuáro não existe!!!'})
 
     const spot = await Spots.create({
+        thumbnail: filename,
         user: user_id,
         company,
         price,
